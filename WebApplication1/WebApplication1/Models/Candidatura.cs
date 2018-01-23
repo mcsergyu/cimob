8d9e2fdf93c9c21239b6a270d13a30080b7ecca0
Mário Reis <mario.reis@ivirgula.com>
2018-01-23 15:00:55 +0000

    Modelo Candidatura

---
+++ WebApplication1/WebApplication1/Models/Candidatura.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cimob.Models
+{
+    public class Candidatura
+    {
+        [Key]
+        public int CandidaturaId { get; set; }
+
+        [ForeignKey("Program")]
+        [Display(Name = "Programa")]
+        public int ProgramId { get; set; }
+
+        [ForeignKey("User")]
+        [Display(Name = "Candidato")]
+        public int UserId { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Data de submissão")]
+        [Required(ErrorMessage = "A indicação da data é obrigatório")]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Data de último estado")]
+        [Required(ErrorMessage = "A indicação da data é obrigatório")]
+        public DateTime LastStateDate { get; set; }
+
+        public virtual Program AppliedProgram { get; set; }
+        public virtual ApplicationUser SubmissionUser { get; set; }
+    }
+}
