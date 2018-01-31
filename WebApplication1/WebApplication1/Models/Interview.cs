fcce9bbbe1e7e3496880b544cb47cfbd4c7f796d
Mário Reis <mario.reis@ivirgula.com>
2018-01-31 22:04:26 +0000

    Model Interview

---
+++ WebApplication1/WebApplication1/Models/Interview.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cimob.Models
+{
+    public class Interview
+    {
+        public int InterviewId { get; set; }
+
+        [ForeignKey("Candidatura")]
+        [Display(Name = "Candidatura")]
+        public int CandidaturaId { get; set; }
+
+        [Display(Name = "Descrição")]
+        [MaxLength(120, ErrorMessage = "Não pode exceder os 120 caracteres")]
+        public String Description { get; set; }
+
+        [Display(Name = "Data Agendada")]
+        public DateTime StartDate { get; set; }
+
+        public Boolean Confirmed { get; set; }
+    }
+}
