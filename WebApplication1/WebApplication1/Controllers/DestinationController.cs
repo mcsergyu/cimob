86e4a9dd8e7d60da7c0490d4e5afe35c1a07d8d1
mcsergyu <sergiomachado@vodafone.pt>
2018-02-16 15:52:34 +0000

    Comentários
    Comentarios no model e controller do Program

--- WebApplication1/WebApplication1/Controllers/DestinationController.cs
+++ WebApplication1/WebApplication1/Controllers/DestinationController.cs
@@ -52,90 +52,90 @@ namespace Cimob.Controllers
             return View();
         }
 
-        // POST: Destination/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("DestinationId,Cidade,Pais")] Destination destination)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(destination);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(destination);
-        }
+        //// POST: Destination/Create
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create([Bind("DestinationId,Cidade,Pais")] Destination destination)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _context.Add(destination);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(destination);
+        //}
 
-        // GET: Destination/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //// GET: Destination/Edit/5
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var destination = await _context.Destinations.SingleOrDefaultAsync(m => m.DestinationId == id);
-            if (destination == null)
-            {
-                return NotFound();
-            }
-            return View(destination);
-        }
+        //    var destination = await _context.Destinations.SingleOrDefaultAsync(m => m.DestinationId == id);
+        //    if (destination == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(destination);
+        //}
 
-        // POST: Destination/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("DestinationId,Cidade,Pais")] Destination destination)
-        {
-            if (id != destination.DestinationId)
-            {
-                return NotFound();
-            }
+        //// POST: Destination/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("DestinationId,Cidade,Pais")] Destination destination)
+        //{
+        //    if (id != destination.DestinationId)
+        //    {
+        //        return NotFound();
+        //    }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(destination);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!DestinationExists(destination.DestinationId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(destination);
-        }
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(destination);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!DestinationExists(destination.DestinationId))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(destination);
+        //}
 
-        // GET: Destination/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //// GET: Destination/Delete/5
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var destination = await _context.Destinations
-                .SingleOrDefaultAsync(m => m.DestinationId == id);
-            if (destination == null)
-            {
-                return NotFound();
-            }
+        //    var destination = await _context.Destinations
+        //        .SingleOrDefaultAsync(m => m.DestinationId == id);
+        //    if (destination == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(destination);
-        }
+        //    return View(destination);
+        //}
 
         // GET: Destination/Delete/5
         public async Task<IActionResult> DestinationProgram(int? id)
